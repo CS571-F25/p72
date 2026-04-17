9ff46fe51881ea4d03b19e4835bafb7cddadf672
An Lou <louan1734@gmail.com>
2026-04-17 17:11:16 -0500

    real data is good to use

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,1 +1,10 @@
-export { default } from "./HomeAtmosphere";
+import HomeAtmosphere from "./HomeAtmosphere";
+import Weather from "./Weather";
+
+export default function Home() {
+  if (import.meta.env.DEV) {
+    return <HomeAtmosphere />;
+  }
+
+  return <Weather />;
+}
